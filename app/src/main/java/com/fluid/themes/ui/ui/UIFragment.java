a2c787de79bff1f9e9ae66fa17b7fb1e05a4f331
AndroBuddy <anirudhchimpidi8@gmail.com>
2020-06-14 19:29:05 +0530

    Fluid Themes: Hello world!

---
+++ app/src/main/java/com/fluid/themes/ui/ui/UIFragment.java
@@ -0,0 +1,18 @@
+package com.fluid.themes.ui.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.fluid.themes.R;
+
+public class UIFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ui, null);
+    }
+}
